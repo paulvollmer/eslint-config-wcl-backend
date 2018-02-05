effb99121b04910c2fdf1052f3282c27fa6409cb
Paul Vollmer <paul.vollmer@protonmail.com>
2018-02-05 15:05:04 +0100

    Created makefile for common make targets

---
+++ setup.mk
@@ -0,0 +1,17 @@
+BIN ?= ./node_modules/.bin
+DIR ?= $(dir $(lastword $(MAKEFILE_LIST)))
+LINT_SRC_JS ?= .
+LINT_SRC_JSON ?= package.json ./**/*.json
+
+lint: lint-js lint-json
+lint-js:
+	@echo "Linting JavaScript..."
+	@$(BIN)/eslint $(LINT_SRC_JS)
+lint-json:
+	@echo "Linting JSON..."
+	@$(BIN)/jsonlint -q $(LINT_SRC_JSON)
+lint-fix:
+	@echo "Linting JavaScript and try to fix..."
+	@$(BIN)/eslint --fix $(LINT_SRC)
+
+.PHONY: lint lint-js lint-json lint-fix
